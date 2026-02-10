fd8bedd5b85dcfc0ce7e05687ff7793fd70cc82c
AidanSugrue28 <aidan_sugrue28@milton.edu>
2026-02-10 17:37:09 -0500

    2/6

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -24,21 +24,21 @@ import javax.swing.JPanel;
 
 public class BasicGameApp implements Runnable {
 
-   //Variable Definition Section
-   //Declare the variables used in the program 
-   //You can set their initial values too
-   
-   //Sets the width and height of the program window
-	final int WIDTH = 1000;
-	final int HEIGHT = 700;
+    //Variable Definition Section
+    //Declare the variables used in the program
+    //You can set their initial values too
 
-   //Declare the variables needed for the graphics
-	public JFrame frame;
-	public Canvas canvas;
+    //Sets the width and height of the program window
+    final int WIDTH = 1000;
+    final int HEIGHT = 700;
+
+    //Declare the variables needed for the graphics
+    public JFrame frame;
+    public Canvas canvas;
     public JPanel panel;
-   
-	public BufferStrategy bufferStrategy;
-	public Image rockPic;
+
+    public BufferStrategy bufferStrategy;
+    public Image rockPic;
     public Image paperPic;
     public Image scissorsPic;
     public Image backgroundPic;
@@ -47,36 +47,38 @@ public class BasicGameApp implements Runnable {
     public Image scissorsWinPic;
     public boolean gameOver = false;
     public String winner  = "";
+    public Image PaperWinsPic;
 
 
     //Declare the objects used in the program
-   //These are things that are made up of more than one variable type
-	public Rock rock1;
+    //These are things that are made up of more than one variable type
+    public Rock rock1;
     public Paper paper1;
     public Scissors scissors1;
+    public PaperWins PaperWins;
 
 
 
-   // Main method definition
-   // This is the code that runs first and automatically
-	public static void main(String[] args) {
-		BasicGameApp ex = new BasicGameApp();   //creates a new instance of the game
-		new Thread(ex).start();                 //creates a threads & starts up the code in the run( ) method  
-	}
+    // Main method definition
+    // This is the code that runs first and automatically
+    public static void main(String[] args) {
+        BasicGameApp ex = new BasicGameApp();   //creates a new instance of the game
+        new Thread(ex).start();                 //creates a threads & starts up the code in the run( ) method
+    }
 
 
-   // Constructor Method
-   // This has the same name as the class
-   // This section is the setup portion of the program
-   // Initialize your variables and construct your program objects here.
-	public BasicGameApp() {
-      
-      setUpGraphics();
-       
-      //variable and objects
-      //create (construct) the objects needed for the game and load up
-		rockPic = Toolkit.getDefaultToolkit().getImage("rock.jpg"); //load the picture
-		rock1 = new Rock((int)(Math.random()*700)+1,(int)(Math.random()*500)+1);
+    // Constructor Method
+    // This has the same name as the class
+    // This section is the setup portion of the program
+    // Initialize your variables and construct your program objects here.
+    public BasicGameApp() {
+
+        setUpGraphics();
+
+        //variable and objects
+        //create (construct) the objects needed for the game and load up
+        rockPic = Toolkit.getDefaultToolkit().getImage("rock.jpg"); //load the picture
+        rock1 = new Rock((int)(Math.random()*700)+1,(int)(Math.random()*500)+1);
         paperPic = Toolkit.getDefaultToolkit().getImage("paper.png"); //load the picture
         paper1 = new Paper((int)(Math.random()*700)+1,(int)(Math.random()*500)+1);
         scissorsPic = Toolkit.getDefaultToolkit().getImage("scissors.jpg"); //load the picture
@@ -90,29 +92,29 @@ public class BasicGameApp implements Runnable {
 
     }// BasicGameApp()
 
-   
+
 //*******************************************************************************
 //User Method Section
 //
 // put your code to do things here.
 
-   // main thread
-   // this is the code that plays the game after you set things up
-	public void run() {
+    // main thread
+    // this is the code that plays the game after you set things up
+    public void run() {
 
-      //for the moment we will loop things forever.
-		while (true) {
+        //for the moment we will loop things forever.
+        while (true) {
 
-         moveThings();  //move all the game objects
-         render();  // paint the graphics
-         pause(20); // sleep for 10 ms
-		}
-	}
+            moveThings();  //move all the game objects
+            render();  // paint the graphics
+            pause(20); // sleep for 10 ms
+        }
+    }
 
 
-	public void moveThings()
-	{
-      //calls the move( ) code in the objects
+    public void moveThings()
+    {
+        //calls the move( ) code in the objects
         if (!gameOver) {
             rock1.move();
             paper1.move();
@@ -120,7 +122,7 @@ public class BasicGameApp implements Runnable {
             crashing();
         }
 
-	}
+    }
     public void crashing() {
 
         if (paper1.hitbox.intersects(rock1.hitbox)&& rock1.isAlive == true) {
@@ -160,54 +162,54 @@ public class BasicGameApp implements Runnable {
     }
 
 
-	
-   //Pauses or sleeps the computer for the amount specified in milliseconds
-   public void pause(int time ){
-   		//sleep
-			try {
-				Thread.sleep(time);
-			} catch (InterruptedException e) {
 
-			}
-   }
+    //Pauses or sleeps the computer for the amount specified in milliseconds
+    public void pause(int time ){
+        //sleep
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
 
-
-   //Graphics setup method
-   private void setUpGraphics() {
-      frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
-   
-      panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
-      panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
-      panel.setLayout(null);   //set the layout
-   
-      // creates a canvas which is a blank rectangular area of the screen onto which the application can draw
-      // and trap input events (Mouse and Keyboard events)
-      canvas = new Canvas();  
-      canvas.setBounds(0, 0, WIDTH, HEIGHT);
-      canvas.setIgnoreRepaint(true);
-   
-      panel.add(canvas);  // adds the canvas to the panel.
-   
-      // frame operations
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
-      frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
-      frame.setResizable(false);   //makes it so the frame cannot be resized
-      frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
-      
-      // sets up things so the screen displays images nicely.
-      canvas.createBufferStrategy(2);
-      bufferStrategy = canvas.getBufferStrategy();
-      canvas.requestFocus();
-      System.out.println("DONE graphic setup");
-   
-   }
+        }
+    }
 
 
+    //Graphics setup method
+    private void setUpGraphics() {
+        frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
 
-	//paints things on the screen using bufferStrategy
-	private void render() {
-		Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
-		g.clearRect(0, 0, WIDTH, HEIGHT);
+        panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
+        panel.setLayout(null);   //set the layout
+
+        // creates a canvas which is a blank rectangular area of the screen onto which the application can draw
+        // and trap input events (Mouse and Keyboard events)
+        canvas = new Canvas();
+        canvas.setBounds(0, 0, WIDTH, HEIGHT);
+        canvas.setIgnoreRepaint(true);
+
+        panel.add(canvas);  // adds the canvas to the panel.
+
+        // frame operations
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
+        frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
+        frame.setResizable(false);   //makes it so the frame cannot be resized
+        frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
+
+        // sets up things so the screen displays images nicely.
+        canvas.createBufferStrategy(2);
+        bufferStrategy = canvas.getBufferStrategy();
+        canvas.requestFocus();
+        System.out.println("DONE graphic setup");
+
+    }
+
+
+
+    //paints things on the screen using bufferStrategy
+    private void render() {
+        Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+        g.clearRect(0, 0, WIDTH, HEIGHT);
         g.drawImage(backgroundPic, 0, 0, WIDTH,HEIGHT , null);
 
 
@@ -240,6 +242,6 @@ public class BasicGameApp implements Runnable {
 
         g.dispose();
 
-		bufferStrategy.show();
-	}
+        bufferStrategy.show();
+    }
 }
